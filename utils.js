1bea87922550876eea9b3f24d48c360f0841c8b0
Julian <jumpingafterrain@gmail.com>
2022-10-12 19:02:37 +0200

    proof of concept working

--- utils.js
+++ utils.js
@@ -38,13 +38,3 @@ export async function DiscordRequest(endpoint, options) {
   // return original response
   return res;
 }
-
-// Simple method that returns a random emoji from list
-export function getRandomEmoji() {
-  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
-  return emojiList[Math.floor(Math.random() * emojiList.length)];
-}
-
-export function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
